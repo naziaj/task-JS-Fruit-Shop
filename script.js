9c6bc698cc2a185bd9e037ff96c5601cfbb3fc52
naziaj <49521887+naziaj@users.noreply.github.com>
2019-06-18 12:08:03 +0500

    WIP

---
+++ script.js
@@ -0,0 +1,25 @@
+function applePrice(){
+    let costOfApple= 10;
+    console.log('cost of 1 apple is;')
+    console.log(costOfApple);
+console.log('cost of 2 apple is;') 
+console.log(costOfApple*2);
+console.log('cost of 3 apple is;')
+console.log(costOfApple*3);
+console.log('cost of 4 apple is;')
+console.log(costOfApple*4);
+console.log('cost of 5 apple is;')
+console.log(costOfApple*5);
+console.log('cost of 6 apple is;')
+console.log(costOfApple*6);
+console.log('cost of 7 apple is;')
+console.log(costOfApple*7);
+console.log('cost of 8 apple is;')
+console.log(costOfApple*8);
+console.log('cost of 9 apple is;')
+console.log(costOfApple*9);
+}
+function applePrice(){
+    let lessApple= document.querySelector('input');
+   console.log('lessApple')
+}
